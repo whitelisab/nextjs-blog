82e212518ea4676903d0f3b690f05813e377433e
Lisa White <whitelisab@gmail.com>
2021-12-12 16:00:34 -0700

    Fix Link import in layout component

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
-import Link from 'next/Link';
+import Link from 'next/link';
 
 const name = 'Lisa White';
 export const siteTitle = 'Next.js Sample Website';
