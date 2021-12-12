85475956780a1223209c04836fbede00776bb327
Lisa White <whitelisab@gmail.com>
2021-12-12 16:36:31 -0700

    remove old first-post file, add typescript, convert files to ts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,9 +3,10 @@ import Layout from '../../components/layout';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
+import {GetStaticProps, GetStaticPaths} from 'next';
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params })=> {
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData
@@ -13,7 +14,7 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -21,7 +22,13 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({ postData }: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  }
+}) {
   return (
     <Layout>
       <Head>
